6ba5deae0a21236d5d6c0d2968e0fca6fbcadffb
Khlebovitch Thomas <tkhlebovitch@hotmail.com>
2020-06-18 22:56:37 +0200

    Create "Blinking LED" TypeScript code

---
+++ blinking-led/README.md
@@ -0,0 +1,7 @@
+# blinking-led
+This small project makes an LED blink.
+The original JavaScript code was taken from [w3schools](https://www.w3schools.com/nodejs/nodejs_raspberrypi_blinking_led.asp), 
+but we adapted it, so that we can use pure TypeScript.
+
+## Prerequisites
+### Components
---
+++ blinking-led/src/index.ts
@@ -0,0 +1,56 @@
+import { Gpio } from 'onoff';
+
+/**
+ * Defines the pin we want to use
+ */
+const pinNumber = 24;   // We want to use GPIO pin 24
+
+/**
+ * Defines the pin as an output
+ */
+const LED = new Gpio(pinNumber, 'out');
+
+/**
+ * Defines how often the LED should blink (in ms)
+ */
+const blinkTime = 200;  // every 200ms
+
+/**
+ * Defines when the blinking should stop (in ms)
+ */
+const endTime = 10000; // after 10 seconds
+
+/**
+ * Toggles the state of a GPIO pin
+ * The pin can either be on ("1") or off ("0")
+ * If the pin is on (high), then turn it off (low); otherwise the other way around
+ *
+ * @param pin the GPIO pin whose state should be toggled
+ */
+function toggleState(pin: Gpio): void {
+    const pinState = pin.readSync();
+    let outputState;
+    if (pinState === Gpio.HIGH) {
+        outputState = Gpio.LOW;
+    } else {
+        outputState = Gpio.HIGH;
+    }
+    pin.writeSync(outputState);
+}
+
+/**
+ * Ends the toggling for the given pin
+ *
+ * @param pin the GPIO pin, whose state is currently toggled
+ * @param interval the interval which should be cleared
+ */
+function endToggling(pin: Gpio, interval: number): void {
+    clearInterval(interval);
+    // Turn the pin off
+    pin.writeSync(Gpio.LOW);
+    // Free resources
+    pin.unexport();
+}
+
+const blinkInterval = setInterval(() => toggleState(LED), blinkTime);
+setTimeout(() => endToggling(LED, blinkInterval), endTime);
