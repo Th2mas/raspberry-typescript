cbc3dff27e6f98c87b7c8e4067c44057694d05a3
Khlebovitch Thomas <tkhlebovitch@hotmail.com>
2020-07-28 00:28:10 +0200

    Tetris - Add testing for most of the existing methods

--- tetris/spec/game.spec.ts
+++ tetris/spec/game.spec.ts
@@ -1,39 +1,273 @@
-import "jasmine";
-import { Game } from '../src/game';
-import {Polyomino} from '../src/blocks/polyomino';
-import {TETROMINO_DEFINITION} from '../src/blocks/definitions/tetromino.definition';
+import 'jasmine';
+import {Game} from '../src/game';
 
 describe('Game', () => {
+    const emptyMatrix = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+    let positionMatrix: Array<number>;
+    let matrix: Array<number>;
+
+    beforeEach(() => {
+        // Reset matrix
+        positionMatrix = [...emptyMatrix];
+        matrix = [...emptyMatrix];
+    });
+
     describe('moveDown', () => {
-        let matrix: Array<number>;
-        let polyomino: Polyomino;
-
-        beforeEach(() => {
-            // Reset matrix
-            matrix = [];
-            for (let i = 0; i < 8; i++) {
-                matrix.push(0x00);
-            }
-
-            // Get random polyomino
-            polyomino = new Polyomino(TETROMINO_DEFINITION);
-
-            // Push it to the matrix
-            polyomino.getPosition().forEach((val, idx) => matrix[idx] = val);
-        });
-
         it('should move the current position one row down', () => {
             // Arrange
-            const matrixBefore = [...matrix];
+            positionMatrix[6] = 0xF0;
+            const matrixBefore = [...positionMatrix];
 
             // Act
-            Game.moveDown(matrix);
+            Game.moveDown(positionMatrix);
 
             // Assert
-            expect(matrix.length).toBe(matrixBefore.length);
-            matrix.forEach((row, idx) =>
-                expect(matrix[idx]).toEqual(idx > 0 ? matrixBefore[idx - 1] : 0)
+            positionMatrix.forEach((row, idx) =>
+                expect(positionMatrix[idx]).toEqual(idx > 0 ? matrixBefore[idx - 1] : 0)
             );
+        });
+    });
+
+    describe('isAbleToMoveDown', () => {
+        it('should allow to move down, if we are not in the last row', () => {
+            // Arrange
+            positionMatrix[6] = 0xF0;
+
+            // Act
+            const isAbleToMoveDown = Game.isAbleToMoveDown(positionMatrix, matrix);
+
+            // Assert
+            expect(isAbleToMoveDown).toBe(true);
+        });
+
+        it('should not allow to move down, if we are in the last row', () => {
+            // Arrange
+            positionMatrix[7] = 0xF0;
+
+            // Act
+            const isAbleToMoveDown = Game.isAbleToMoveDown(positionMatrix, matrix);
+
+            // Assert
+            expect(isAbleToMoveDown).toBe(false);
+        });
+
+        it('should not allow to move down, if there is an obstacle below the block', () => {
+            // Arrange
+            positionMatrix[6] = 0xF0;
+            matrix[7] = 0xF0;
+
+            // Act
+            const isAbleToMoveDown = Game.isAbleToMoveDown(positionMatrix, matrix);
+
+            // Assert
+            expect(isAbleToMoveDown).toBe(false);
+        });
+    });
+
+    describe('moveRight', () => {
+        it('should move the current position one column to the right', () => {
+            // Arrange
+            positionMatrix[3] = 0x40;   // 01000000
+            positionMatrix[4] = 0x40;   // 01000000
+            positionMatrix[5] = 0x40;   // 01000000
+            positionMatrix[6] = 0x40;   // 01000000
+
+            const secondPositionMatrix = [...emptyMatrix];
+            secondPositionMatrix[6] = 0x78; // 01111000
+
+            // Act
+            Game.moveRight(positionMatrix, matrix);
+            Game.moveRight(secondPositionMatrix, matrix);
+
+            // Assert
+            expect(positionMatrix[3]).toEqual(0x20);        // 00100000
+            expect(positionMatrix[4]).toEqual(0x20);        // 00100000
+            expect(positionMatrix[5]).toEqual(0x20);        // 00100000
+            expect(positionMatrix[6]).toEqual(0x20);        // 00100000
+
+            expect(secondPositionMatrix[6]).toEqual(0x3C);  // 00111100
+        });
+    });
+
+    describe('isAbleToMoveRight', () => {
+        it('should not allow to move to the right, if there is an obstacle next to the block', () => {
+            // Arrange
+            positionMatrix[6] = 0x4;    // 00000100
+            matrix[6] = 0x2;            // 00000010
+
+            // Act
+            const isAbleToMoveRight = Game.isAbleToMoveRight(positionMatrix, matrix);
+
+            // Assert
+            expect(positionMatrix.length).toBe(matrix.length);
+            expect(isAbleToMoveRight).toBe(false);
+        });
+
+        it('should not allow to move to the right, if we are in most right column', () => {
+            // Arrange
+            positionMatrix[7] = 0x0F;       // 00001111
+
+            // Act
+            const isAbleToMoveRight = Game.isAbleToMoveRight(positionMatrix, matrix);
+
+            // Assert
+            expect(positionMatrix.length).toBe(matrix.length);
+            expect(isAbleToMoveRight).toBe(false);
+        });
+
+        it('should allow to move right, if we are not on the most right column', () => {
+            // Arrange
+            positionMatrix[4] = 0x60;   // 01100000
+
+            // Act
+            const isAbleToMoveRight = Game.isAbleToMoveRight(positionMatrix, matrix);
+
+            // Assert
+            expect(positionMatrix.length).toBe(matrix.length);
+            expect(isAbleToMoveRight).toBe(true);
+        });
+    });
+
+    describe('moveLeft', () => {
+        it('should move the current position one column to the left', () => {
+            // Arrange
+            positionMatrix[3] = 0x40;   // 01000000
+            positionMatrix[4] = 0x40;   // 01000000
+            positionMatrix[5] = 0x40;   // 01000000
+            positionMatrix[6] = 0x40;   // 01000000
+
+            const secondPositionMatrix = [...emptyMatrix];
+            secondPositionMatrix[6] = 0x78; // 01111000
+
+            // Act
+            Game.moveLeft(positionMatrix, matrix);
+            Game.moveLeft(secondPositionMatrix, matrix);
+
+            // Assert
+            expect(positionMatrix[3]).toEqual(0x80);        // 10000000
+            expect(positionMatrix[4]).toEqual(0x80);        // 10000000
+            expect(positionMatrix[5]).toEqual(0x80);        // 10000000
+            expect(positionMatrix[6]).toEqual(0x80);        // 10000000
+
+            expect(secondPositionMatrix[6]).toEqual(0xF0);  // 11110000
+        });
+    });
+
+    describe('isAbleToMoveLeft', () => {
+        it('should not allow to move to the left, if there is an obstacle next to the block', () => {
+            // Arrange
+            positionMatrix[6] = 0x2;    // 00000010
+            matrix[6] = 0x4;            // 00000100
+
+            // Act
+            const isAbleToMoveLeft = Game.isAbleToMoveLeft(positionMatrix, matrix);
+
+            // Assert
+            expect(positionMatrix.length).toBe(matrix.length);
+            expect(isAbleToMoveLeft).toBe(false);
+        });
+
+        it('should not allow to move to the left, if we are on most left column', () => {
+            // Arrange
+            positionMatrix[6] = 0x90;    // 10010000
+
+            // Act
+            const isAbleToMoveLeft = Game.isAbleToMoveLeft(positionMatrix, matrix);
+
+            // Assert
+            expect(positionMatrix.length).toBe(matrix.length);
+            expect(isAbleToMoveLeft).toBe(false);
+        });
+
+        it('should allow to move left, if we are not on the most left column', () => {
+            // Arrange
+            positionMatrix[4] = 0x60;   // 01100000
+
+            // Act
+            const isAbleToMoveLeft = Game.isAbleToMoveLeft(positionMatrix, matrix);
+
+            // Assert
+            expect(positionMatrix.length).toBe(matrix.length);
+            expect(isAbleToMoveLeft).toBe(true);
+        });
+    });
+
+    describe('hasReachedTop', () => {
+        it('should be true, if the position matrix has no space left to place blocks', () => {
+            // Arrange
+            for (let i = 0; i < positionMatrix.length; i++) {
+                positionMatrix[i] = 0x80;
+            }
+
+            // Act
+            const hasReachedTop = Game.hasReachedTop(positionMatrix);
+
+            // Assert
+            expect(hasReachedTop).toBe(true);
+        });
+        it('should be false, if the position matrix still has space for putting blocks', () => {
+            // Arrange
+            const maxIndex = positionMatrix.length - 1;
+            for (let i = maxIndex - 1; i >= 0; i--) {
+                positionMatrix[i] = 0x80;
+            }
+
+            // Act
+            const hasReachedTop = Game.hasReachedTop(positionMatrix);
+
+            // Assert
+            expect(hasReachedTop).toBe(false);
+        });
+    });
+
+    describe('hasPlaceToPut', () => {
+        it('should be true for a block, which still fits in the matrix', () => {
+            // Arrange
+            for (let i = 0; i < 4; i++) {
+                positionMatrix[i] = 0x80;
+            }
+
+            // Act
+            const hasPlaceToPut = Game.hasPlaceToPut(positionMatrix, matrix);
+
+            // Assert
+            expect(hasPlaceToPut).toBe(true);
+        });
+        it('should be false for a block, which cannot find in the matrix', () => {
+            // Arrange
+            for (let i = 0; i < 4; i++) {
+                positionMatrix[i] = 0x80;
+            }
+
+            for (let i = 2; i < matrix.length; i++) {
+                matrix[i] = 0x80;
+            }
+
+            // Act
+            const hasPlaceToPut = Game.hasPlaceToPut(positionMatrix, matrix);
+
+            // Assert
+            expect(hasPlaceToPut).toBe(false);
+        });
+    });
+
+    describe('saveCurrentPosition', () => {
+        it('should store the value from the positionMatrix to the saveMatrix', () => {
+            // Arrange
+            for (let i = positionMatrix.length / 2; i < positionMatrix.length; i++) {
+                positionMatrix[i] = 0x80;
+            }
+            matrix[6] = 0x0F;   // 00001111
+            matrix[7] = 0x0A;   // 00001010
+
+            // Act
+            Game.saveCurrentPosition(positionMatrix, matrix);
+
+            // Assert
+            expect(matrix[4]).toEqual(0x80);    // 10000000
+            expect(matrix[5]).toEqual(0x80);    // 10000000
+            expect(matrix[6]).toEqual(0x8F);    // 10001111
+            expect(matrix[7]).toEqual(0x8A);    // 10001010
         });
     });
 });
--- tetris/src/game.ts
+++ tetris/src/game.ts
@@ -39,13 +39,23 @@ export class Game {
 
     private subscriptions = [];
 
+    private static readonly EMPTY_MATRIX = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+
     constructor() {
         this.definition = TETROMINO_DEFINITION;
 
         activateControlButtons();
         this.subscriptions.push(
-            moveLeft$.subscribe(async () => await this.moveLeft()),
-            moveRight$.subscribe(async () => await this.moveRight()),
+            moveLeft$.subscribe(async () => {
+                // Since the LEDs are reversed, we need to call moveRight here
+                Game.moveRight(this.positionMatrix, this.matrix);
+                await Game.writeLEDS(this.positionMatrix, this.matrix);
+            }),
+            moveRight$.subscribe(async () => {
+                // Since the LEDs are reversed, we need to call moveRight here
+                Game.moveLeft(this.positionMatrix, this.matrix);
+                await Game.writeLEDS(this.positionMatrix, this.matrix);
+            }),
             rotateLeft$.subscribe(async () => await this.rotateLeft()),
             rotateRight$.subscribe(async () => await this.rotateRight())
         );
@@ -61,7 +71,7 @@ export class Game {
     private newGame() {
         this.movementSpeed = Game.DEFAULT_MOVING_TIME;
         this.savedBlocks = 0;
-        this.matrix = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+        this.matrix = [...Game.EMPTY_MATRIX];
         this.points = 0;
 
         this.resetPolyomino();
@@ -71,22 +81,24 @@ export class Game {
         (async () => {
             while (true) {
                 try {
-                    await this.writeLEDS();
+                    await Game.writeLEDS(this.positionMatrix, this.matrix);
 
                     // Check, if we are already in the lowest row
-                    if (this.isAbleToMoveDown()) {
+                    if (Game.isAbleToMoveDown(this.positionMatrix, this.matrix)) {
                         // TODO: Something is still wrong with the move down method
                         // Sometimes the LEDs still merge together, although they shouldn't. Maybe it's the isAbleToMoveDown method?
                         Game.moveDown(this.positionMatrix);
                     } else {
-                        if (this.hasReachedTop()) {
+                        if (Game.hasReachedTop(this.matrix) || !Game.hasPlaceToPut(this.positionMatrix, this.matrix)) {
                             console.info(`Game over! Collected points: ${this.points}`);
                             console.info('To close the game, press Ctrl+C');
                             this.subscriptions.forEach(subscription => subscription.unsubscribe());
                             deactivateControlButtons();
                             return;
                         }
-                        this.saveCurrentPosition();
+
+                        Game.saveCurrentPosition(this.positionMatrix, this.matrix);
+                        this.savedBlocks++;
                         this.updateMovementSpeed();
                         this.resetPolyomino();
                     }
@@ -104,53 +116,10 @@ export class Game {
 
     private resetPolyomino(): void {
         this.polyomino = new Polyomino(this.definition);
-        this.positionMatrix = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+        this.positionMatrix = [...Game.EMPTY_MATRIX];
+
+        // Per default, the position is on the most right
         this.polyomino.getPosition().forEach((val, idx) => this.positionMatrix[idx] = val);
-    }
-
-    private async moveRight(): Promise<void> {
-        await this.move(num => num << 1, num => num);
-    }
-
-    private async moveLeft(): Promise<void> {
-        await this.move(num => num >> 1, num => ~(num - 1) & 0xFF);  // Invert the bits for a simpler check, result should be < 255
-    }
-
-    private async move(shift: (num: number) => number, checkValue: (num: number) => number): Promise<void> {
-        let oldMatrix = [...this.positionMatrix];
-        let isValidShift = true;
-
-        // Shift the position only where it is really needed
-        this.getNonZeroIndices()
-            .forEach(idx => {
-                const shiftedValue = shift(this.positionMatrix[idx]);
-                isValidShift = isValidShift && (checkValue(shiftedValue) < 0xFF);
-                if (isValidShift) {
-                    this.positionMatrix[idx] = shiftedValue;
-                }
-            });
-
-        // If we are overlapping with the save matrix or have performed a shift too much (i.e. we went outside the allowed range), restore
-        if (this.isOverlapping() || !isValidShift) {
-            this.positionMatrix = oldMatrix;
-        }
-
-        await this.writeLEDS();
-    }
-
-    private async writeLEDS(): Promise<void> {
-        await resetAll();
-
-        for (let row = 0; row < this.matrix.length; row++) {
-            await writeLedNumber(row, this.matrix[row] | this.positionMatrix[row]);
-        }
-    }
-
-    /**
-     * Checks, if the current position of the polyomino is overlapping with the values inside the save matrix
-     */
-    private isOverlapping(): boolean {
-        return this.matrix.some((value, index) => (value & this.positionMatrix[index]) !== 0);
     }
 
     /**
@@ -162,36 +131,95 @@ export class Game {
     }
 
     /**
+     * Checks, if the current polyomino is able to move one row down
+     */
+    static isAbleToMoveDown(positionMatrix: Array<number>, matrix: Array<number>): boolean {
+        const indexOfLowestBlocksRow = Game.getIndexOfLowestBlocksRow(positionMatrix);
+
+        // Checks, if there is still a row available to move down
+        const isNotOnTheLowestRow = indexOfLowestBlocksRow !== (matrix.length - 1);
+
+        // Check, if the current polyomino position is not overlapping with the next row of the tetris matrix
+        const isNotOverlappingWithNextRow = (positionMatrix[indexOfLowestBlocksRow] & matrix[indexOfLowestBlocksRow + 1]) === 0;
+
+        return isNotOnTheLowestRow && isNotOverlappingWithNextRow;
+    }
+
+    static moveRight(positionMatrix: Array<number>, matrix: Array<number>): void {
+        Game.move(positionMatrix, matrix, Game.isAbleToMoveRight, val => val >> 1);
+    }
+
+    static moveLeft(positionMatrix: Array<number>, matrix: Array<number>): void {
+        Game.move(positionMatrix, matrix, Game.isAbleToMoveLeft, val => val << 1);
+    }
+
+    static isAbleToMoveRight(positionMatrix: Array<number>, matrix: Array<number>): boolean {
+        return Game.isAbleToMove(positionMatrix, matrix, num => num >> 1, 0x01);
+    }
+
+    static isAbleToMoveLeft(positionMatrix: Array<number>, matrix: Array<number>): boolean {
+        return Game.isAbleToMove(positionMatrix, matrix, num => num << 1, 0x80);
+    }
+
+    private static isAbleToMove(
+        positionMatrix: Array<number>,
+        matrix: Array<number>,
+        shift: (num: number) => number,
+        maskValue: number
+    ): boolean {
+        const indicesOfNonZeroBlocks = Game.getNonZeroIndices(positionMatrix);
+
+        let isGoingToOverlapWithMatrix = false;
+        let isGoingOutOfBounds = false;
+
+        for (const idx of indicesOfNonZeroBlocks) {
+            isGoingToOverlapWithMatrix = isGoingToOverlapWithMatrix || ((shift(positionMatrix[idx]) & matrix[idx]) !== 0);
+            isGoingOutOfBounds = isGoingOutOfBounds || ((positionMatrix[idx] & maskValue) !== 0);
+        }
+
+        return !isGoingToOverlapWithMatrix && !isGoingOutOfBounds;
+    }
+
+    private static move(
+        positionMatrix: Array<number>,
+        matrix: Array<number>,
+        check: (positionMatrix: Array<number>, matrix: Array<number>) => boolean,
+        shift: (num: number) => number
+    ): void {
+        if (check(positionMatrix, matrix)) {
+            // We want to assign the shifted matrix to the current one. Since we only pass references, we use splice for setting it directly
+            positionMatrix.splice(0, positionMatrix.length, ...positionMatrix.map(shift));
+        }
+    }
+
+    private static async writeLEDS(positionMatrix: Array<number>, matrix: Array<number>): Promise<void> {
+        await resetAll();
+
+        for (let row = 0; row < matrix.length; row++) {
+            await writeLedNumber(row, matrix[row] | positionMatrix[row]);
+        }
+    }
+
+    /**
      * Saves the current position of the position matrix in the save matrix
      */
-    private saveCurrentPosition(): void {
-        this.getNonZeroIndices()
-            .forEach(idx => this.matrix[idx] = this.matrix[idx] | this.positionMatrix[idx]); // Save the position
-        this.savedBlocks++;
+    static saveCurrentPosition(positionMatrix: Array<number>, matrix: Array<number>): void {
+        matrix.splice(0, matrix.length, ...positionMatrix
+            .map((val, idx) => matrix[idx] | val));
+    }
+
+    static hasPlaceToPut(positionMatrix: Array<number>, matrix: Array<number>): boolean {
+        return Game.getNonZeroIndices(positionMatrix)
+            .reduce((prev, curr) => prev && ((positionMatrix[curr] & matrix[curr]) === 0), true);
     }
 
     /**
      * Gets the indices of the position matrix, where the value is not 0 -> get the parts where the polyomino is stored
      */
-    private getNonZeroIndices(): Array<number> {
-        return this.positionMatrix
+    static getNonZeroIndices(matrix: Array<number>): Array<number> {
+        return matrix
             .map((val, idx) => val !== 0 ? idx : -1)    // Get only the indices, where the value is not 0x00
             .filter(val => val !== -1);                                 // Filter out redundant indices
-    }
-
-    /**
-     * Checks, if the current polyomino is able to move one row down
-     */
-    private isAbleToMoveDown(): boolean {
-        const indexOfLowestBlocksRow = this.getIndexOfLowestBlocksRow();
-
-        // Checks, if there is still a row available to move down
-        const isNotOnTheLowestRow = indexOfLowestBlocksRow !== (this.matrix.length - 1);
-
-        // Check, if the current polyomino position is not overlapping with the next row of the tetris matrix
-        const isNotOverlappingWithNextRow = (this.positionMatrix[indexOfLowestBlocksRow] & this.matrix[indexOfLowestBlocksRow + 1]) === 0;
-
-        return isNotOnTheLowestRow && isNotOverlappingWithNextRow;
     }
 
     /**
@@ -199,15 +227,15 @@ export class Game {
      * It requires, that the position matrix already has a polyomino inside
      * @return the index of the lowest blocks
      */
-    private getIndexOfLowestBlocksRow(): number {
-        return (this.positionMatrix.length - [...this.positionMatrix].reverse().findIndex(num => num !== 0)) - 1;
+    private static getIndexOfLowestBlocksRow(matrix: Array<number>): number {
+        return (matrix.length - [...matrix].reverse().findIndex(num => num !== 0)) - 1;
     }
 
     /**
      * Checks, if the top was reached and therefore if the game is over
      */
-    private hasReachedTop(): boolean {
-        return this.matrix.reduce((prev, curr) => prev && (curr !== 0), true);
+    static hasReachedTop(matrix: Array<number>): boolean {
+        return matrix.reduce((prev, curr) => prev && (curr !== 0), true);
     }
 
     private updateMovementSpeed(): void {
@@ -219,14 +247,12 @@ export class Game {
         this.polyomino.rotateCounterClockWise();
         // TODO: Rotation does not work yet
         console.log('Rotate left');
-        await this.writeLEDS();
     }
 
     private async rotateRight(): Promise<void> {
         this.polyomino.rotateClockWise();
         // TODO: Rotation does not work yet
         console.log('Rotate right');
-        await this.writeLEDS();
     }
 
     /**
